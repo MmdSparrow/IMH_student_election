4bbc5ca470eef2f1f7f159f0dadecb313eb26a5c
Sayed Mohammad Ali Mirkazemi <esmirk.137@gmail.com>
2022-04-10 07:33:24 +0430

    regex pattern

--- src/main/java/ir/blacksparrow/imh_student_election/view/viewDto/permission/viewDto/PermissionViewDto.java
+++ src/main/java/ir/blacksparrow/imh_student_election/view/viewDto/permission/viewDto/PermissionViewDto.java
@@ -5,9 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.Pattern;
+
 @ToString
 @NoArgsConstructor
 @Data
 public class PermissionViewDto {
+
+    @Pattern(regexp="/[a-z]*:(write|read|delete|put)$/gm",message="title must be of the regex form: /[a-z]*:(write|read|delete|put)$/gm")
     private String title;
 }
